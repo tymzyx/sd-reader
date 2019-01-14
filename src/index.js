8f62a9a2b543a43f1500a3b8c8c2180d78ec67d9
tymzyx <httsongli@163.com>
2019-01-14 13:57:56 +0800

    add eruda for debug of mobile

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import './assets/svg/index';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
+    const eruda = require('eruda');
+    eruda.init();
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
